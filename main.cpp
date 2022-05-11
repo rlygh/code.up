96098ca902d123457d814173013f578bcc7d90a9
rlygh <97500244+rlygh@users.noreply.github.com>
2022-05-11 07:20:08 +0000

    시,분,초 입력 /분 출력

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    int a,b,c,d,e;
-    scanf("%1d%1d%1d%1d%1d",&a,&b,&c,&d,&e);
-    printf("[%d]\n[%d]\n[%d]\n[%d]\n[%d]",a*10000, b*1000,c*100,d*10,e);
+    int h,m,s;
+    scanf("%d:%d:%d",&h,&m,&s);
+    printf("%d",m);
 
     return 0;
 }
